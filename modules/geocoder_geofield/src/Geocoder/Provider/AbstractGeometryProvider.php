81d300e0d50a6c6a709c2fb86d5c0934ba035837
itamair <itamair@me.com>
2019-01-12 08:30:46 -0500

    Implementation of GeometryProviderInterface for \Geometry returning providers (GPX, KML, Geojson). Refactoring/ Better declaration of PhpDocs to admit the return of \Geometry for Providers implementing GeometryProviderInterface.

--- modules/geocoder_geofield/src/Geocoder/Provider/AbstractGeometryProvider.php
+++ modules/geocoder_geofield/src/Geocoder/Provider/AbstractGeometryProvider.php
@@ -3,14 +3,13 @@
 namespace Drupal\geocoder_geofield\Geocoder\Provider;
 
 use Geocoder\Exception\NoResult;
+use Geometry;
 use Geocoder\Exception\UnsupportedOperation;
-use Geocoder\Provider\AbstractProvider;
-use Geocoder\Provider\Provider;
 
 /**
  * Provides an abstract file handler to be used by GeoPHP Wrapper.
  */
-abstract class GeoPhpProvider extends AbstractProvider implements Provider {
+abstract class AbstractGeometryProvider implements GeometryProviderInterface {
 
   /**
    * Geophp interface.
@@ -30,24 +29,23 @@ abstract class GeoPhpProvider extends AbstractProvider implements Provider {
    * {@inheritdoc}
    */
   public function __construct() {
-    parent::__construct();
     $this->geophp = \Drupal::service('geofield.geophp');
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getName() {
+  public function getName(): string {
     return 'geophp_provider';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function geocode($filename) {
+  public function geocode($filename): Geometry {
     if (file_exists($filename)) {
       $geophp_string = file_get_contents($filename);
-      /* @var \Geometry|\GeometryCollection $geometry */
+      /* @var \Geometry $geometry */
       $geometry = $this->geophp->load($geophp_string, $this->geophpType);
       if (!empty($geometry->components) || $geometry instanceof \Geometry) {
         return $geometry;
