d4eda4e5e942f2dca7d09ff88beb0369b508c00f
itamair <itamair@1179076.no-reply.drupal.org>
2019-06-13 22:59:00 -0400

    Issue #3060535 by bradjones1, itamair: GoogleMapsBusiness plugin breaks 3.x config entity upgrade path

--- src/Plugin/Geocoder/Provider/GoogleMapsBusiness.php
+++ src/Plugin/Geocoder/Provider/GoogleMapsBusiness.php
@@ -3,10 +3,7 @@
 namespace Drupal\geocoder\Plugin\Geocoder\Provider;
 
 use Drupal\geocoder\ConfigurableProviderUsingHandlerWithAdapterBase;
-use Drupal\Core\Cache\CacheBackendInterface;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Http\Client\HttpClient;
+use Geocoder\Provider\GoogleMaps\GoogleMaps;
 
 /**
  * Geocoder provider plugin for Google Maps for Business.
@@ -27,30 +24,20 @@ use Http\Client\HttpClient;
 class GoogleMapsBusiness extends ConfigurableProviderUsingHandlerWithAdapterBase {
 
   /**
-   * GoogleMapsBusiness constructor.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The config factory service.
-   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
-   *   The cache backend used to cache geocoding data.
-   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
-   *   The Drupal language manager service.
-   * @param \Http\Client\HttpClient $http_adapter
-   *   The HTTP adapter.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   *
-   * @throws \Exception
+   * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory, CacheBackendInterface $cache_backend, LanguageManagerInterface $language_manager, HttpClient $http_adapter) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $config_factory, $cache_backend, $language_manager, $http_adapter);
-    throw new \Exception('Google Maps for Business needs to be instantiated by calling \Geocoder\Provider\GoogleMaps\GoogleMaps::business()');
+  protected function getHandler() {
+    if ($this->handler === NULL) {
+      $this->handler = GoogleMaps::business(
+        $this->httpAdapter,
+        $this->configuration['clientId'],
+        $this->configuration['privateKey'],
+        $this->configuration['region'],
+        $this->configuration['apiKey'],
+        $this->configuration['channel']
+      );
+    }
+    return $this->handler;
   }
 
 }
