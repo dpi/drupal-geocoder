6771ae823ebeb9bb1dcee1951e450c0366a3779c
karens <karens@45874.no-reply.drupal.org>
2020-05-31 21:15:14 +0200

    Issue #3144108 by KarenS: Improvements to Geocoder Address geocoding

--- modules/geocoder_address/src/Plugin/Field/FieldFormatter/AddressGeocodeFormatter.php
+++ modules/geocoder_address/src/Plugin/Field/FieldFormatter/AddressGeocodeFormatter.php
@@ -6,6 +6,16 @@ use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\geocoder_field\Plugin\Field\FieldFormatter\GeocodeFormatter;
 use Geocoder\Model\AddressCollection;
 use Drupal\Component\Plugin\Exception\PluginException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\geocoder\GeocoderInterface;
+use Drupal\geocoder\ProviderPluginManager;
+use Drupal\geocoder\DumperPluginManager;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Utility\LinkGeneratorInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\geocoder_address\AddressService;
 
 /**
  * Plugin implementation of the Geocode formatter.
@@ -19,6 +29,91 @@ use Drupal\Component\Plugin\Exception\PluginException;
  * )
  */
 class AddressGeocodeFormatter extends GeocodeFormatter {
+
+  /**
+   * The address service.
+   *
+   * @var Drupal\geocoder_address\AddressService
+   */
+  protected $addressService;
+
+  /**
+   * Constructs a GeocodeFormatterBase object.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings.
+   * @param \Drupal\geocoder\GeocoderInterface $geocoder
+   *   The gecoder service.
+   * @param \Drupal\geocoder\ProviderPluginManager $provider_plugin_manager
+   *   The provider plugin manager service.
+   * @param \Drupal\geocoder\DumperPluginManager $dumper_plugin_manager
+   *   The dumper plugin manager service.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer.
+   * @param \Drupal\Core\Utility\LinkGeneratorInterface $link_generator
+   *   The Link Generator service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger factory.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param Drupal\geocoder_address\AddressService $address_service
+   *   The Geocoder Address service.
+   */
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    $label,
+    $view_mode,
+    array $third_party_settings,
+    GeocoderInterface $geocoder,
+    ProviderPluginManager $provider_plugin_manager,
+    DumperPluginManager $dumper_plugin_manager,
+    RendererInterface $renderer,
+    LinkGeneratorInterface $link_generator,
+    LoggerChannelFactoryInterface $logger_factory,
+    EntityTypeManagerInterface $entity_type_manager,
+    AddressService $address_service
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $geocoder, $provider_plugin_manager, $dumper_plugin_manager, $renderer, $link_generator, $logger_factory, $entity_type_manager);
+    $this->addressService = $address_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('geocoder'),
+      $container->get('plugin.manager.geocoder.provider'),
+      $container->get('plugin.manager.geocoder.dumper'),
+      $container->get('renderer'),
+      $container->get('link_generator'),
+      $container->get('logger.factory'),
+      $container->get('entity_type.manager'),
+      $container->get('geocoder_address.address')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -35,15 +130,8 @@ class AddressGeocodeFormatter extends GeocodeFormatter {
 
     foreach ($items as $delta => $item) {
       $value = $item->getValue();
-      $address = [];
-
-      $address[] = !empty($value['address_line1']) ? $value['address_line1'] : NULL;
-      $address[] = !empty($value['address_line2']) ? $value['address_line2'] : NULL;
-      $address[] = !empty($value['postal_code']) ? $value['postal_code'] : NULL;
-      $address[] = !empty($value['locality']) ? $value['locality'] : NULL;
-      $address[] = !empty($value['country']) ? $value['country'] : NULL;
-
-      if ($address_collection = $this->geocoder->geocode(implode(' ', array_filter($address)), $providers)) {
+      $address_string = $this->addressService->addressArrayToGeoString($value);
+      if ($address_collection = $this->geocoder->geocode($address_string, $providers)) {
         $elements[$delta] = [
           '#markup' => $address_collection instanceof AddressCollection && !$address_collection->isEmpty() ? $dumper->dump($address_collection->first()) : "",
         ];
